887bbfa199c236b78d6411d18dbe7c4abcdc8917
rajithr <r.rajith@gmail.com>
2016-01-20 18:43:10 +0800

    Update DP.java

--- DP.java
+++ DP.java
@@ -3,12 +3,29 @@ public class DP {
 	{
 		int num = 0;
 			
+		num = handle10(start, end, dp, num);
+		
+		num = handle100(end, dp, num);
+		
+		num = handleK(end, dp, num);
+		
+		num = handle10K(end, dp, num);
+		
+		num = handleLakh(end, dp, num);
+		
+		return num;
+	}
+
+	private static int handle10(int start, int end, int dp, int num) {
 		for(int n=start;(n<10 && n<=end);n++)
 		{
 			if(n==dp)
 				num++;
 		}
-		
+		return num;
+	}
+
+	private static int handle100(int end, int dp, int num) {
 		for(int n=11;(n<100 && n<=end);n++)
 		{
 			int tens = n / 10;
@@ -16,7 +33,10 @@ public class DP {
 			if(tens * unit == dp)
 				num++;
 		}
-		
+		return num;
+	}
+
+	private static int handleK(int end, int dp, int num) {
 		for(int n=101;(n<1000 && n<=end);n++)
 		{
 			int hundreds = n /100;
@@ -25,7 +45,10 @@ public class DP {
 			if(hundreds * tens * unit == dp)
 				num++;
 		}
-		
+		return num;
+	}
+
+	private static int handle10K(int end, int dp, int num) {
 		for(int n=1001;(n<10000 && n<=end);n++)
 		{
 			int thousands = n / 1000;
@@ -35,7 +58,10 @@ public class DP {
 			if(thousands * hundreds * tens * unit == dp)
 				num++;
 		}
-		
+		return num;
+	}
+
+	private static int handleLakh(int end, int dp, int num) {
 		for(int n=10001;(n<100000 && n<=end);n++)
 		{
 			int ten_thousands = n / 10000;
@@ -46,7 +72,6 @@ public class DP {
 			if(ten_thousands * thousands * hundreds * tens * unit == dp)
 				num++;
 		}
-		
 		return num;
 	}
 }
