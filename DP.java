e02348558cc013d582828a484e7f719e24720f39
rajithr <r.rajith@gmail.com>
2016-01-21 12:42:42 +0800

    Update DP.java

--- DP.java
+++ DP.java
@@ -5,13 +5,13 @@ public class DP {
 			
 		num = handle10(start, end, dp, num);
 		
-		num = handle100(end, dp, num);
+		num = handle100(start, end, dp, num);
 		
-		num = handleK(end, dp, num);
+		num = handleK(start, end, dp, num);
 		
-		num = handle10K(end, dp, num);
+		num = handle10K(start, end, dp, num);
 		
-		num = handleLakh(end, dp, num);
+		num = handleLakh(start, end, dp, num);
 		
 		return num;
 	}
@@ -25,8 +25,10 @@ public class DP {
 		return num;
 	}
 
-	private static int handle100(int end, int dp, int num) {
-		for(int n=11;(n<100 && n<=end);n++)
+	private static int handle100(int start, int end, int dp, int num) {
+		if(start < 11)
+			start = 11;
+		for(int n=start;(n<100 && n<=end);n++)
 		{
 			int tens = n / 10;
 			int unit = n % 10;
@@ -36,8 +38,10 @@ public class DP {
 		return num;
 	}
 
-	private static int handleK(int end, int dp, int num) {
-		for(int n=101;(n<1000 && n<=end);n++)
+	private static int handleK(int start, int end, int dp, int num) {
+		if(start < 101)
+			start = 101;
+		for(int n=start;(n<1000 && n<=end);n++)
 		{
 			int hundreds = n /100;
 			int tens = (n % 100) / 10;
@@ -48,8 +52,10 @@ public class DP {
 		return num;
 	}
 
-	private static int handle10K(int end, int dp, int num) {
-		for(int n=1001;(n<10000 && n<=end);n++)
+	private static int handle10K(int start, int end, int dp, int num) {
+		if(start < 1001)
+			start = 1001;
+		for(int n=start;(n<10000 && n<=end);n++)
 		{
 			int thousands = n / 1000;
 			int hundreds = (n%1000) /100;
@@ -61,8 +67,10 @@ public class DP {
 		return num;
 	}
 
-	private static int handleLakh(int end, int dp, int num) {
-		for(int n=10001;(n<100000 && n<=end);n++)
+	private static int handleLakh(int start, int end, int dp, int num) {
+		if(start < 10001)
+			start = 10001;
+		for(int n=start;(n<100000 && n<=end);n++)
 		{
 			int ten_thousands = n / 10000;
 			int thousands = (n%10000) / 1000;
