43092e19fc9e71c3de8cd399105e5ff73c38bf24
rajithr <r.rajith@gmail.com>
2016-01-21 14:42:18 +0800

    Update DP.java

--- DP.java
+++ DP.java
@@ -71,16 +71,16 @@ public class DP {
 		for(int n=start;(n<100000 && n<=end);n++)
 		{
 			int ten_thousands = n / 10000;
-			if(ten_thousands == 0)
+			if(n < 10000 && ten_thousands == 0)
 				ten_thousands = 1;
 			int thousands = (n%10000) / 1000;
-			if(thousands == 0)
+			if(n < 1000 && thousands == 0)
 				thousands = 1;
 			int hundreds = (n%1000) /100;
-			if(hundreds == 0)
+			if(n < 100 && hundreds == 0)
 				hundreds = 1;
 			int tens = (n % 100) / 10;
-			if(tens == 0)
+			if(n < 10 && tens == 0)
 				tens = 1;
 			int unit = n % 10;
 			if(ten_thousands * thousands * hundreds * tens * unit == dp)
