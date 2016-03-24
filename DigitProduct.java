16bda6372e31dcef678340743eb5172dd59d731a
rajithr <r.rajith@gmail.com>
2016-03-24 12:44:06 +0800

    Update DigitProduct.java

--- DigitProduct.java
+++ DigitProduct.java
@@ -2,7 +2,7 @@ public class DigitProduct implements DP.DigitP
 {
   public double getDP(String number)
   {
-    int product = 1;
+    int product = 2;
     
     for(int n=0;n<number.length();n++)
       product *= Integer.parseInt(number.substring(n,n+1));
